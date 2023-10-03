6e7568520cfd4558365e3a84a97cf99707f2e9dc
KostiantynPolishko1 <kostantinpolishko@gmail.com>
2023-10-03 17:31:52 +0300

    7 Edited function "increaseArrRowCol" with it's subfunctions. Added function "icrementCount".

--- Point2D.cpp
+++ Point2D.cpp
@@ -16,49 +16,54 @@ void Point2D::setCol(short Col) {
 	_col = Col;
 }
 ;
-void Point2D::initArrRowCol(short** arr)
-{
+void Point2D::incrementCount() {
 	_count++;
-	arr = new short* [_count];
+}
+;
+short** Point2D::initArrRowCol()
+{
+	short** arr = new short* [_count];
 
 	for (short i = 0; i < _count; i++)
 		arr[i] = new short[2];
+
+	return arr;
 }
 ;
-void Point2D::copyArrRowCol(short** arr, short** arr2)
+void Point2D::copyArrRowCol(short** arr, short** arr2, short count)
 {
-	for (short i = 0; i < _count; i++) {
+	for (short i = 0; i < count; i++) {
 		arr[i][0] = arr2[i][0];
 		arr[i][1] = arr2[i][1];
 	}
 }
 ;
-void Point2D::deleteArrRowCol(short** arr)
+void Point2D::deleteArrRowCol(short** arr, short count)
 {
-	for (short i = 0; i < _count; i++) {
+	for (short i = 0; i < count; i++) {
 		delete[] arr[i];
 		arr[i] = nullptr;
 	}
 
-	delete[] _arrRowCol;
-	_arrRowCol = nullptr;
+	delete[] arr;
+	arr = nullptr;
 }
 ;
 void Point2D::increaseArrRowCol() {
 
-	short** arrTemp = nullptr;
-	initArrRowCol(arrTemp);
+	short** arrTemp = initArrRowCol();
 
-	copyArrRowCol(arrTemp, _arrRowCol);
+	copyArrRowCol(arrTemp, _arrRowCol, _count);
 
-	deleteArrRowCol(_arrRowCol);
+	deleteArrRowCol(_arrRowCol, _count);
 
-	initArrRowCol(_arrRowCol);
+	incrementCount();
+	_arrRowCol = initArrRowCol();
 
-	copyArrRowCol(_arrRowCol, arrTemp);
+	copyArrRowCol(_arrRowCol, arrTemp, _count-1);
 	_arrRowCol[_count - 1][0] = _row;
 	_arrRowCol[_count - 1][1] = _col;
 
-	deleteArrRowCol(arrTemp);
+	deleteArrRowCol(arrTemp, _count - 1);
 }
 ;
